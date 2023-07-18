485f31742a6961094c23d55cd37d03a7b624f441
Lil' Cranky <59399356+moonway-dev@users.noreply.github.com>
2023-07-18 19:07:28 +0300

    upload scripts

---
+++ System4TT.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCREW.Auth.System4TT
+{
+    public class System4TT
+    {
+        public string GenerateSecretKey()
+        {
+            string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_=+[]{}|;:,.<>?";
+            StringBuilder codeBuilder = new StringBuilder();
+            Random random = new Random();
+
+            for (int i = 0; i < 64; i++)
+            {
+                int index = random.Next(chars.Length);
+                codeBuilder.Append(chars[index]);
+            }
+
+            return codeBuilder.ToString();
+        }
+
+        public string EncodeSecretKey(string secretKey)
+        {
+            byte[] bytesToEncode = System.Text.Encoding.UTF8.GetBytes(secretKey);
+            string base64String = Convert.ToBase64String(bytesToEncode);
+            return base64String;
+        }
+
+        public string DecodeSecretKey(string base64Key)
+        {
+            byte[] bytesToDecode = Convert.FromBase64String(base64Key);
+            string decodedString = System.Text.Encoding.UTF8.GetString(bytesToDecode);
+            return decodedString;
+        }
+    }
+}
---
+++ TwoFactorAuthenticator.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCREW.Auth.System4TT
+{
+    public class TwoFactorAuthenticator
+    {
+        private const int AuthenticationCodeLength = 6;
+        private readonly byte[] secretKeyBytes;
+
+        public TwoFactorAuthenticator(string secretKey)
+        {
+            secretKeyBytes = Encoding.ASCII.GetBytes(secretKey);
+        }
+
+        private long GetCurrentUnixTimestamp()
+        {
+            return DateTimeOffset.UtcNow.ToUnixTimeSeconds() / 100 * 100;
+        }
+
+        private byte[] GetHmacSha1(byte[] keyBytes, byte[] data)
+        {
+            using (HMACSHA1 hmac = new HMACSHA1(keyBytes))
+            {
+                return hmac.ComputeHash(data);
+            }
+        }
+
+        private int GetDynamicTruncation(byte[] hmacSha1)
+        {
+            int offset = hmacSha1[hmacSha1.Length - 1] & 0x0F;
+            return ((hmacSha1[offset] & 0x7F) << 24) |
+                   ((hmacSha1[offset + 1] & 0xFF) << 16) |
+                   ((hmacSha1[offset + 2] & 0xFF) << 8) |
+                   (hmacSha1[offset + 3] & 0xFF);
+        }
+
+        private string GenerateOTP(long unixTimestamp)
+        {
+            byte[] timestampBytes = BitConverter.GetBytes(unixTimestamp);
+
+            if (BitConverter.IsLittleEndian)
+                Array.Reverse(timestampBytes);
+
+            byte[] hmacSha1 = GetHmacSha1(secretKeyBytes, timestampBytes);
+            int otpValue = GetDynamicTruncation(hmacSha1) % 1000000;
+            return otpValue.ToString("D6");
+        }
+
+        public string GenerateAuthenticationCode()
+        {
+            long unixTimestamp = GetCurrentUnixTimestamp();
+            return GenerateOTP(unixTimestamp);
+        }
+
+        public bool VerifyAuthenticationCode(string userProvidedCode)
+        {
+            long unixTimestamp = GetCurrentUnixTimestamp();
+            for (int i = -30; i <= 30; i++)
+            {
+                string expectedCode = GenerateOTP(unixTimestamp + i);
+                if (userProvidedCode == expectedCode)
+                    return true;
+            }
+            return false;
+        }
+    }
+}
